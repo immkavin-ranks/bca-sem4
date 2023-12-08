ee5f7c4b68583d48ee7968fb25d30d6e8bfc9bd3
Kavin <immkavin.ofcl@gmail.com>
2023-12-08 02:47:14 +0000

    Hello: R

---
+++ r-examples/hello.R
@@ -0,0 +1,7 @@
+# Generate some sample data
+x <- rnorm(100)
+y <- rnorm(100)
+
+# Create the scatter plot with labels and title
+plot(x, y, xlab = "X", ylab = "Y", main = "Scatter Plot of X vs. Y")
+print("hello, world")
