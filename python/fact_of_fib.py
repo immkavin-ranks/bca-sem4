36148da257b3c20569b53ccf57281e8e83c4e108
Kavin <immkavin.ofcl@gmail.com>
2023-12-20 12:18:40 +0000

    Wed, Dec 20, 2023, 12:18 PM +00:00

--- python/fact_of_fib.py
+++ python/fact_of_fib.py
@@ -6,4 +6,25 @@ def main():
     for i in range(n):
         fib_list.append(fib(i))
 
-    for 
+    for item in fib_list:
+        fact_list.append(fact(item))
+
+    print(fib_list)
+    print(fact_list)
+
+
+def fib(n):
+    if n == 0:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def fact(a):
+    if a == 0 or a == 1:
+        return 1
+    else:
+        return a * fact(a - 1)
+
+
+main()
