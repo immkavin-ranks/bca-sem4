6b95875b6c8fefb6c759da6d111a861a636796ad
Kavin <immkavin.ofcl@gmail.com>
2024-01-01 06:37:27 +0000

    Add trick multiline comment

---
+++ r-examples/Hello.R
@@ -0,0 +1,8 @@
+#Trick for multi-line comment  
+if(FALSE) {  
+   "R is an interpreted computer programming language which was created by  
+   Ross Ihaka and Robert Gentleman at the University of Auckland, New Zealand "  
+}  
+#My First program in R programming  
+string <-"Hello World!"  
+print(string)
