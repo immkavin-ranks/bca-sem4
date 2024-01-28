3b4a193cfaa0b4e23551339de9b4aeab64209834
Kavin <immkavin.ofcl@gmail.com>
2024-01-28 05:25:16 +0000

    Solve phoneNumber problem original solution

--- r-examples/PhoneNumber.R
+++ r-examples/PhoneNumber.R
@@ -1,16 +1,18 @@
-parse_phone_number <- function(number_string) {
-  # \b\w{2-9}%\w{0-9}\b
-  num_strsplit <- strsplit(number_string, "")[[1]]
-  output <- c()
-  for (c in num_strsplit) {
-    if (c in 1:9) {
-      output <- c(output, c)
-    }
-  }
+# https://exercism.org/tracks/r/exercises/phone-number/
 
-  outputR
-  num_strsplit
+parse_phone_number <- function(number_string) {
+  number <- gsub("[^0-9]", "", number_string)
+  if (substr(number, 1, 1) == "1" && nchar(number) == 11) {
+    number <- substr(number, 2, 11)
+  } else if (nchar(number) < 10 || nchar(number) > 10 || substr(number, 1, 1) < 2 || substr(number, 4, 4) < 2) {
+    number <- NULL
+  }
+  number
 }
 
 
-parse_phone_number("2-409501-12")
+parse_phone_number("1-4095043(243)")
+parse_phone_number("+1 (613)-995-0253")
+parse_phone_number("613-995-0253")
+parse_phone_number("1 613 995 0253")
+parse_phone_number("613.995.0253")
